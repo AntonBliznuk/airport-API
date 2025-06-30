8190d445b646ef5066141541cb9bed1235470e7a
Антон <antonbliznuk71@gmail.com>
2025-06-30 09:09:27 +0300

    create models "Airplane" and "AirplaneType" in app "airport" and register it in the admin panel.

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Airplane Type"
+        verbose_name_plural = "Airplane Types"
+        ordering = ["name"]
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=63)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
+    )
+
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return f"{self.name}(seats:{self.capacity()}, type: {self.airplane_type.name})"
+
+    class Meta:
+        verbose_name = "Airplane"
+        verbose_name_plural = "Airplanes"
+        ordering = ["name", "rows", "seats_in_row"]
