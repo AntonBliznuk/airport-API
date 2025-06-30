4ea883a422a38f6719b4e0354ff6df5f45fe7956
Антон <antonbliznuk71@gmail.com>
2025-06-30 09:42:41 +0300

    create models "Airport" and "Route" in app "airport", add migrations for the models and register it in the admin panel.

--- airport/admin.py
+++ airport/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from airport.models import Airplane, AirplaneType
-
+from airport.models import Airplane, AirplaneType, Route, Airport
 
 admin.site.register(AirplaneType)
 admin.site.register(Airplane)
+admin.site.register(Airport)
+admin.site.register(Route)
--- airport/models.py
+++ airport/models.py
@@ -31,3 +31,37 @@ class Airplane(models.Model):
         verbose_name = "Airplane"
         verbose_name_plural = "Airplanes"
         ordering = ["name", "rows", "seats_in_row"]
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=63, unique=True)
+    closest_big_city = models.CharField(max_length=63)
+
+    def __str__(self):
+        return f"{self.name}(city: {self.closest_big_city})"
+
+    class Meta:
+        verbose_name = "Airport"
+        verbose_name_plural = "Airports"
+        ordering = ["name"]
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="sources"
+    )
+    destination = models.ForeignKey(
+        Airport, on_delete=models.CASCADE, related_name="destinations"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return (
+            f"{self.source.name}({self.source.closest_big_city})"
+            f" -> {self.destination.name}({self.destination.closest_big_city})"
+        )
+
+    class Meta:
+        verbose_name = "Route"
+        verbose_name_plural = "Routes"
+        ordering = ["source", "destination", "distance"]
