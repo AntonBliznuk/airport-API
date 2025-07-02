a6b1e73eef42365c414ffab1da3bec831d6bd599
Антон <antonbliznuk71@gmail.com>
2025-07-02 10:09:00 +0300

    Serializer, ViewSet were implemented for model "AirplaneType". View set was registered in airport/urls.py. DEFAULT_PAGINATION_CLASS was added to REST_FRAMEWORK settings in settings.py.

--- airport/urls.py
+++ airport/urls.py
@@ -1,5 +1,15 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+from airport.views import (
+    AirplaneTypeViewSet,
+)
+
+router = DefaultRouter()
+router.register("airplane-types", AirplaneTypeViewSet)
 
 app_name = "airport"
 
-urlpatterns = []
+urlpatterns = [
+    path("", include(router.urls)),
+]
--- airport/views.py
+++ airport/views.py
@@ -1,2 +1,15 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from airport.models import (
+    AirplaneType,
+)
+from airport.serializers import (
+    AirplaneTypeSerializer,
+)
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+    permission_classes = [IsAdminUser]
